e7b8de17faac12f3c9a4b9f5a7e0efef33e041ea
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-08-12 23:03:28 +0300

    Initial Commit

---
+++ WeeklyPulseTests/WeeklyPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyPulseTests.swift
+//  WeeklyPulseTests
+//
+//  Created by Eyüp Erdoğan on 12.08.2024.
+//
+
+import Testing
+@testable import WeeklyPulse
+
+struct WeeklyPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ WeeklyPulseUITests/WeeklyPulseUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WeeklyPulseUITestsLaunchTests.swift
+//  WeeklyPulseUITests
+//
+//  Created by Eyüp Erdoğan on 12.08.2024.
+//
+
+import XCTest
+
+final class WeeklyPulseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
