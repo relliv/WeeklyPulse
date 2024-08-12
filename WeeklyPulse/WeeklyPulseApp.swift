67e5c8934e4622be63966a3917a2f12cb652129e
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-08-12 23:03:28 +0300

    Initial Commit

---
+++ WeeklyPulse/WeeklyPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyPulseApp.swift
+//  WeeklyPulse
+//
+//  Created by Eyüp Erdoğan on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
